f81eabd4ed854576f78236a07875f32409034e58
tianfeng <tf7353@163.com>
2018-04-16 14:18:59 +0800

    no

--- src/com/cn/tf/reflection/GetClass.java
+++ src/com/cn/tf/reflection/GetClass.java
@@ -22,5 +22,11 @@ public class GetClass {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
+
+//        注意：在运行期间，一个类，只有一个Class对象产生,(注:Student可以new出很多对象,别混淆了)
+//
+//        三种方式常用第三种，第一种对象都有了还要反射干什么。第二种需要导入类的包，依赖太强，不导包就抛编译错误。一般都第三种，一个字符串可以传入也可写在配置文件中等多种方法。
+
     }
 }
