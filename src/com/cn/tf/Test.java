b04753a594d1889099e50abb0f3cc080bedd3daf
tianfeng <workingemail:tianfeng@winchannel.net>
2018-12-05 10:38:18 +0800

    提交合并

--- src/com/cn/tf/Test.java
+++ src/com/cn/tf/Test.java
@@ -238,21 +238,34 @@ public class Test {
 		BigDecimal b = new BigDecimal("7.66");
 		System.out.println(b.longValue()+"|"+b.doubleValue());
 		Object o = new Object();
-
-
-
+	}
+	@org.junit.Test
+	public void test11(){
+		//如果找到第一个匹配,而没有break,则会把后边的代码都走一遍
+            int i = 1;
+            switch(i){
+                case 0:
+                    System.out.println("0");
+                case 1:
+                    System.out.println("1");
+                case 2:
+                    System.out.println("2");
+                default:
+                    System.out.println("default");
+            }
+            //System.err.println( JSONObject.toJSONString(returnMap) );
 	}
 	public static String getCustomerInfo(Map<String,String> customerInfoMap) {
 
-		if(customerInfoMap.isEmpty())
-			return "{}";
+		if(customerInfoMap.isEmpty()){
+			return "{}";}
 		StringBuffer sf = new StringBuffer("{");
 		for(Iterator<String> it = customerInfoMap.keySet().iterator(); it.hasNext();){
 			String key = it.next();
 			String value = customerInfoMap.get(key);
 			sf.append(key).append("=").append(value);
-			if(it.hasNext())
-				sf.append("&");
+			if(it.hasNext()){
+				sf.append("&");}
 		}
 		String customerInfo = sf.append("}").toString();
 		return customerInfo;
